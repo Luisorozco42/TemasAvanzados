70eec70575730a4600bdfefffe8c0bf15c0d0d99
Luisorozco42 <lorozcolazo@gmail.com>
2026-05-02 23:36:08 -0600

    writing on a file

--- src/archivos/EditarArchivo.java
+++ src/archivos/EditarArchivo.java
@@ -1,4 +1,26 @@
 package archivos;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class EditarArchivo {
+    static void main() {
+        boolean anexar = false;// esto es para no sobreescribir
+        var nombreArchivo = "mi_archivo.tx";
+        var archivo = new File(nombreArchivo);
+
+        try{
+            //revisar si el archivo existe
+            anexar = archivo.exists();
+            var salida = new PrintWriter(new FileWriter(archivo, anexar));
+            var nuevoContenido = "Nuevo\nContenido";
+            salida.println(nuevoContenido);
+            salida.close();
+            System.out.println("Se afrefo contenido al archivo!");
+        }catch (Exception e){
+            System.out.println("Error al escribir en ek archivo" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
